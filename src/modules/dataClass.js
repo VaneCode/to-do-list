5bd7aaa978bb75c917fa76fd11b640813649bbc5
vanessaBonito <vanebonito@hotmail.com>
2022-07-12 17:49:50 -0500

    Set an array of tasks objects

---
+++ src/modules/dataClass.js
@@ -0,0 +1,16 @@
+//Importe modules
+import TaskClass from "./taskClass.js";
+
+let tasks = [];
+
+const task1 = new TaskClass('Task1',false,0);
+const task2 = new TaskClass('Task2',false,1);
+const task3 = new TaskClass('Task3',false,2);
+const task4 = new TaskClass('Task1',false,3);
+const task5 = new TaskClass('Task1',false,4);
+
+tasks.push(task1);
+tasks.push(task2);
+tasks.push(task3);
+tasks.push(task4);
+tasks.push(task5);
