fad959e20e20c1551bcc500d36431e9559246f3d
vanessaBonito <vanebonito@hotmail.com>
2022-07-13 15:22:18 -0500

    Update addTaskData method in DataClass

--- src/modules/dataClass.js
+++ src/modules/dataClass.js
@@ -11,18 +11,6 @@ export default class DataClass {
       localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 
-    const task1 = new TaskClass('Task1', false, 0);
-    const task2 = new TaskClass('Task2', false, 1);
-    const task3 = new TaskClass('Task3', false, 2);
-    const task4 = new TaskClass('Task1', false, 3);
-    const task5 = new TaskClass('Task1', false, 4);
-
-    tasks.push(task1);
-    tasks.push(task2);
-    tasks.push(task3);
-    tasks.push(task4);
-    tasks.push(task5);
-
     return tasks;
   }
 
@@ -30,12 +18,7 @@ export default class DataClass {
     const tasks = DataClass.getTasks();
 
     // Calculate index
-    let index = 0;
-    if (tasks.length > 1) {
-      index = tasks[tasks.length - 1] + 1;
-    } else {
-      index = 1;
-    }
+    const index = tasks.length + 1;
 
     // Create new task object
     const newTask = new TaskClass(description, completed, index);
