f2738d3c72937189603f41a0b19ffbd3031925df
vanessaBonito <vanebonito@hotmail.com>
2022-07-13 15:58:08 -0500

    Create CRUD methods in DataClass

--- src/modules/dataClass.js
+++ src/modules/dataClass.js
@@ -11,34 +11,17 @@ export default class DataClass {
       localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 
-    const task1 = new TaskClass('Task1', false, 0);
-    const task2 = new TaskClass('Task2', false, 1);
-    const task3 = new TaskClass('Task3', false, 2);
-    const task4 = new TaskClass('Task1', false, 3);
-    const task5 = new TaskClass('Task1', false, 4);
-
-    tasks.push(task1);
-    tasks.push(task2);
-    tasks.push(task3);
-    tasks.push(task4);
-    tasks.push(task5);
-
     return tasks;
   }
 
-  static addTaskData(description, completed) {
+  static addTaskData(description) {
     const tasks = DataClass.getTasks();
 
     // Calculate index
-    let index = 0;
-    if (tasks.length > 1) {
-      index = tasks[tasks.length - 1] + 1;
-    } else {
-      index = 1;
-    }
+    const index = tasks.length + 1;
 
     // Create new task object
-    const newTask = new TaskClass(description, completed, index);
+    const newTask = new TaskClass(description, false, index);
     // Add the new task object to the tasks array
     tasks.push(newTask);
     localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -47,6 +30,24 @@ export default class DataClass {
   static removeTaskData(i) {
     let tasks = DataClass.getTasks();
     tasks = tasks.filter((taks) => Number(taks.index) !== Number(i));
+    // Update remain items indexes
+    for (let j = i; j < tasks.length; j += 1) {
+      tasks[j].index -= 1;
+    }
+    // Local storage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  static updateTaskData(task) {
+    const tasks = DataClass.getTasks();
+
+    tasks.forEach((item, i) => {
+      if (item.index === task.index) {
+        tasks[i].description = task.description;
+        tasks[i].completed = task.completed;
+      }
+    });
+
     // Local storage
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }
